c261f0d8d7de22a5e7b2cbd3706dc5f83527370f
Amit Nandy <nandyamit.in@gmail.com>
2024-09-09 19:31:51 -0700

    update reservetable code

--- js/reservationconfirmation.js
+++ js/reservationconfirmation.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     //select variable to select required elements.
     const userInfo = document.getElementById("userInfo");
-    const randomNumberSpan = document.getElementById("randomNumber");
+   
     
     //grab inputs from local storage     
     const userData = JSON.parse(localStorage.getItem("userData"));
@@ -12,14 +12,12 @@ document.addEventListener('DOMContentLoaded', function() {
         userInfo.innerHTML = `
             <p><strong>Name:</strong> ${userData.name}</p>
             <p><strong>Email:</strong> ${userData.email}</p>
-            <p><strong>Message:</strong> ${userData.message}</p>
+            <p><strong>Table ${userData.tableNumber} has been reserved for you! <strong></p>
         `;
     } else {
     //publish data from local storage as inner HTML
         userInfo.innerHTML = "<p>No user data found.</p>";
     }
 
-    //assign variable that shows the table number using random number. 
-    const randomNumber = Math.floor(Math.random() * 10) + 1;
-    randomNumberSpan.textContent = randomNumber;
+    
 });
