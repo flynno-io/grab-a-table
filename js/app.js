bd299d6f6e9355d59e5e2647e34220c7965ca676
flynno <luke.laz.flynn@gmail.com>
2024-09-10 21:08:40 -0600

    updated navbar functionality, styles, and URL manipulation

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 // js/app.js
 
+// set up functions for a HTML/JS Single Page Application
 document.addEventListener("DOMContentLoaded", () => {
 	// Caches the container where pages will be swapped
 	const pageContainer = document.getElementById("page-container")
+    
+    // Keep track of the tab with the 'active' class for updating the UI on click
+    let currentActiveTab = document.querySelector(".active")
 
 	// Function to load a specific page
 	function loadPage(page) {
@@ -11,22 +15,60 @@ document.addEventListener("DOMContentLoaded", () => {
 		pageContainer.appendChild(pageElement) // Append the new page to the container
 	}
 
+    // FIXME: Bootstrap issue with add/removing 'active' class
+    // Update which nav link is active
+	function addActiveClass(target) {
+		target.classList.add("active")
+        currentActiveTab.classList.remove("active")
+        currentActiveTab = target
+	}
+
+    // FIXME: take control of the refresh and hard refresh
+    function updateURL(path, id=null) {
+        const getBaseURL = () => {
+            const protocol = window.location.protocol
+            const hostname = window.location.hostname
+            const port = window.location.port ? `:${window.location.port}` : ""
+            return `${protocol}//${hostname}${port}`
+        }
+        const baseURL = getBaseURL()
+        window.history.pushState({},'', `${baseURL}/${path}`)
+    }
+    updateURL('home')
+
 	// Event listeners for navigation buttons
-	document
-		.getElementById("nav-home")
-		.addEventListener("click", () => loadPage("home-page"))
-    document
-        .getElementById("nav-reserve")
-        .addEventListener("click", () => loadPage("reserve-page"))
-	document
-		.getElementById("nav-login")
-		.addEventListener("click", () => loadPage("login-page"))
-    document
-        .getElementById("nav-signup")
-        .addEventListener("click", () => loadPage("login-page"))
+	document.getElementById("nav-home").addEventListener("click", (e) => {
+        e.preventDefault()
+		addActiveClass(e.target)
+		loadPage("home-page")
+        updateURL("home")
+	})
+	document.getElementById("nav-reserve").addEventListener("click", (e) => {
+        e.preventDefault()
+		addActiveClass(e.target)
+		loadPage("reserve-page")
+        updateURL("reserve")
+	})
+	document.getElementById("nav-login").addEventListener("click", (e) => {
+        e.preventDefault()
+		addActiveClass(e.target)
+		loadPage("login-page")
+        updateURL("login")
+	})
+	document.getElementById("nav-signup").addEventListener("click", (e) => {
+        e.preventDefault()
+		addActiveClass(e.target)
+		loadPage("signup-page")
+        updateURL("signup")
+	})
 
 	// TODO: identify how to load the Confirm page after clicking 'submit' on reserve form
 
 	// Load the default page (Home) on initial load
 	loadPage("home-page")
 })
+
+document.addEventListener("beforeunload", (event) => {
+
+});
+
